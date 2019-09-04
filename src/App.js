b909ef4b34547a955b0e53368506b5d2f77326d1
Rr Rr <l9hfighter@gmail.com>
2019-09-04 16:11:20 -0500

    complettteee

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './index.css';
 import TopicBrowser from './components/TopicBrowser/TopicBrowser'
 
-class App extends Component {
+class App extends Component { 
   render() {
     return (
       <TopicBrowser />
